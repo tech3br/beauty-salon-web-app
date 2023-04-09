56fc6608e179b7eb5087598cee0336092cf7b907
Daniel Filgueira <dfsdireito95@gmail.com>
2023-04-09 02:26:47 -0300

    feat: adding menu links mocks

---
+++ src/mocks/menuLinks.ts
@@ -0,0 +1,22 @@
+export const menuLinks = [
+  {
+    title: 'Início',
+    href: '#inicio',
+  },
+  {
+    title: 'Sobre',
+    href: 'sobre',
+  },
+  {
+    title: 'Serviços',
+    href: '#servicos',
+  },
+  {
+    title: 'Depoimentos',
+    href: '#depoimentos',
+  },
+  {
+    title: 'Contato',
+    href: '#contatos',
+  },
+];
