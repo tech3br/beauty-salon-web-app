9675885dede5ed5496e40effa42f2376a7d16cd7
Daniel Filgueira <dfsdireito95@gmail.com>
2023-04-09 03:55:40 -0300

    feat: adding about us section into main page file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 
+import { AboutUs } from '@/components/AboutUs/AboutUs.page';
 import { Header } from '@/components/Header/header.page';
 import { MainCover } from '@/components/MainCover/mainCover.page';
 
@@ -17,6 +18,15 @@ const Index = () => {
           paragraph="Um salão exclusivo em São Paulo, especializado em tratamentos naturais."
           buttonText="Agendar um  horário"
           imageSrc="/assets/images/mainCover-image.png"
+        />
+        <AboutUs
+          heading="Sobre nós"
+          paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sagittis arcu quis nisi luctus, id accumsan felis tristique. Proin quis bibendum diam. Sed consequat nisl laoreet eros ultricies pellentesque. Nullam in est porta, pellentesque massa vitae, vehicula risus. 
+
+          In placerat, felis vitae sodales dictum, lacus quam pretium mi, ut pretium urna turpis eu dui. Vestibulum id ullamcorper nibh. Donec luctus, nunc finibus elementum suscipit, tortor augue vulputate sapien, vitae feugiat enim augue sed. 
+
+          Quisque id aliquam elit. Suspendisse congue pharetra maximus. Duis rutrum velit a leo euismod dictum. Sed sodales est efficitur arcu tincidunt tincidunt. Curabitur fringilla, risus at feugiat feugiat, nisl nulla tincidunt tellus, elementum elementum lorem nisl eleifend dolor. Nullam eget dui at sem ullamcorper luctus."
+          imageSrc="/assets/images/about-us-image.png"
         />
         {/* About Us */}
         {/* Services */}
